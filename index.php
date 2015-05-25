ece9b24f886bd101f6a50177a384c2740d22465a
emanuel12324 <emanuel32804@gmail.com>
2015-05-25 13:16:29 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Platonic Network</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<link href="img/favicon.ico" rel="shortcut icon">
+		<link href="master/master.css" rel="stylesheet">
+	</head>
+	
+	<body>
+		<center>
+			<div class="home-logo">
+				<p class="home-logo">PLATONIC NETWORK</p>
+				<form action="search.php" method="GET">
+					<input type="text" name="q"></input>
+					<input type="submit"></input>
+				</form>
+			</div>
+		</center>
+	</body>
+</html>
